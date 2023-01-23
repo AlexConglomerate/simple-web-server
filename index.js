04c98398c88e3fd815af2f5c301b5ee9c0fac76a
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2023-01-23 20:45:36 +0600

    Добавил кнопку edit

--- index.js
+++ index.js
@@ -7,39 +7,39 @@ const port = 3000
 const app = express()
 
 app.set('view engine', 'ejs')
+
+// Указываем, что страница, которую надо показать, лежит в папке pages. (express ожидает её увидеть в папке views)
 app.set('views', 'pages')
 
-app.use(express.static(path.resolve(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.static(path.resolve(__dirname, 'public'))) // подключаем файл app.js, который лежит в папке public
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', async (req, res) => {
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: false
-  })
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: false
+    })
 })
 
 app.post('/', async (req, res) => {
-  await addNote(req.body.title)
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: true
-  })
+    await addNote(req.body.title)
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: true
+    })
 })
 
 app.delete('/:id', async (req, res) => {
-  await removeNote(req.params.id)
-  res.render('index', {
-    title: 'Express App',
-    notes: await getNotes(),
-    created: false
-  })
+    await removeNote(req.params.id)
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: false
+    })
 })
 
 app.listen(port, () => {
-  console.log(chalk.green(`Server has been started on port ${port}...`))
+    console.log(chalk.green(`Server has been started on port ${port}...`))
 })
--- public/app.js
+++ public/app.js
@@ -1,14 +1,23 @@
 document.addEventListener('click', event => {
-  if (event.target.dataset.type === 'remove') {
-    const id = event.target.dataset.id
+    if (event.target.dataset.type === 'remove') {
+        const id = event.target.dataset.id
 
-    remove(id).then(() => {
-      event.target.closest('li').remove()
-    })
-  }
+        remove(id).then(() => {
+            event.target.closest('li').remove()
+        })
+    }
+
+    if (event.target.dataset.type === 'edit') {
+        const text = event.target.parentNode.parentNode.childNodes[0].textContent.trim()
+        const data = prompt('Enter a new name', text)
+        if (data !== text) {
+            console.log(`Вносим изменения`)
+        }
+    }
 })
 
 
 async function remove(id) {
-  await fetch(`/${id}`, {method: 'DELETE'})
+    await fetch(`/${id}`, {method: 'DELETE'})
 }
+
